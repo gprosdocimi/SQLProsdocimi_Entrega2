67229aeb7f462a0c4d92adba992913ebe010b14f
gprosdocimi <prosdocimigiselle@gmail.com>
2025-05-24 02:56:40 -0300

    SQLScript_2Datos..sql

--- SQLScript_1Datos.sql
+++ SQLScript_1Datos.sql
@@ -15,37 +15,34 @@ INSERT INTO locales(nombre_local, ventas_por_dia, telefono, direccion, fecha_ape
  ("Aturias","140","44427568","Av.Emilio castro 6052","1995-05-13", 2, 2),
  ("Ochavita","100","22345685","Av.Juan B. Alberdi 1556 ","1999-06-21", 2, 1);
 
+
+--Consultar las vistas
 SELECT*FROM vista_barrio_de_locales;
 
 SELECT*FROM vista_locales_info_completa;
 
 SELECT*FROM vista_categoria_de_locales;
 
+--Consultar funciones
 SELECT  id_categoria,
 ventas_por_categoria (id_categoria)
  AS promedio_ventas_categoria 
  FROM locales
  GROUP BY id_categoria;
 
+SELECT cantidad_locales_barrio('Mataderos');
 
-SELECT  id_categoria,
-ventas_por_categoria (id_categoria)
- AS promedio_ventas_categoria 
- FROM locales
- GROUP BY id_categoria;
-
-DELETE FROM locales WHERE id_local= 11;
-
-
+--Ejecutar Stored Procedure
 CALL agregar_local('Don Bianco', 500, 1125589945, 'Av.Larrazabal 1445','1992-05-04');
                
 CALL actualizar_direccion (11, 'Av. Juan Bautista Alberdi 6105');
 
 
-
+---Ejecutar triggers
 DELETE FROM locales WHERE id_local= 13;
 
 SELECT * FROM auditoria_locales;
+
 
 UPDATE locales
 SET Ventas_por_dia = 250
